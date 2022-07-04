517f5468296375fe1e4f9c543ce3d7eab8fe2985
henrylong <henrylong719@gmail.com>
2022-07-04 22:05:15 +0930

    add custom errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there! this is from docket');
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000 ');
